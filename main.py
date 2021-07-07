e941ab59e0eb2218a31924de569f70dc44ff0c07
cx15 <71107658+cx15@users.noreply.github.com>
2021-07-07 17:43:25 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-basic.pause(randint(0, 2000))
-if Math.random_boolean():    
-    basic.show_string("A")
-else:
-    basic.show_string("B")
-basic.pause(1000)
-basic.clear_screen()
+
+
+
+basic.forever(A_or_B)
+def A_or_B():
+    basic.pause(randint(0, 2000))
+    if Math.random_boolean():    
+        basic.show_string("A")
+    else:
+        basic.show_string("B")
+    basic.pause(1000)
+    basic.clear_screen()
 
 
 
--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
-basic.pause(randint(0, 2000))
-if (Math.randomBoolean()) {
-    basic.showString("A")
-} else {
-    basic.showString("B")
-}
-
-basic.pause(1000)
-basic.clearScreen()
+basic.forever(function A_or_B() {
+    basic.pause(randint(0, 2000))
+    if (Math.randomBoolean()) {
+        basic.showString("A")
+    } else {
+        basic.showString("B")
+    }
+    
+    basic.pause(1000)
+    basic.clearScreen()
+})
