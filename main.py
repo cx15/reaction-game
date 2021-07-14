875cf691aabbec914e19868c203714a3a6f1204b
John David Taylor <jaydeetay@users.noreply.github.com>
2021-07-14 17:28:50 -0400

    Plays a high tone when A is pressed at the right time.

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+# Microbit code for a reaction game.
+# Shows a random letter (A or B) for a short length of time.
+# The user must press the corresponding button within that
+# time to score a point.  If they press the wrong button
+# or press too late they lose points.
+letter_showing = ""
 
+def on_button_pressed_a():
+    if letter_showing == "A":
+        music.play_tone(462, music.beat(BeatFraction.WHOLE))
+    else:
+        music.play_tone(162, music.beat(BeatFraction.WHOLE))
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
-
-basic.forever(A_or_B)
-def A_or_B():
+def on_forever():
+    global letter_showing
     basic.pause(randint(0, 2000))
-    if Math.random_boolean():    
+    if Math.random_boolean():
+        letter_showing = "A"
         basic.show_string("A")
     else:
+        letter_showing = "B"
         basic.show_string("B")
     basic.pause(1000)
     basic.clear_screen()
-
-
-
+    letter_showing = ""
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,11 +1,29 @@
-basic.forever(function A_or_B() {
+//  Microbit code for a reaction game.
+//  Shows a random letter (A or B) for a short length of time.
+//  The user must press the corresponding button within that
+//  time to score a point.  If they press the wrong button
+//  or press too late they lose points.
+let letter_showing = ""
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    if (letter_showing == "A") {
+        music.playTone(462, music.beat(BeatFraction.Whole))
+    } else {
+        music.playTone(162, music.beat(BeatFraction.Whole))
+    }
+    
+})
+basic.forever(function on_forever() {
+    
     basic.pause(randint(0, 2000))
     if (Math.randomBoolean()) {
+        letter_showing = "A"
         basic.showString("A")
     } else {
+        letter_showing = "B"
         basic.showString("B")
     }
     
     basic.pause(1000)
     basic.clearScreen()
+    letter_showing = ""
 })
