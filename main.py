bdaf93601457194881920fe4289f7fc81f8c7b4f
John David Taylor <jaydeetay@users.noreply.github.com>
2021-09-15 21:56:27 -0400

    Paste over the top with John's files.

--- main.py
+++ main.py
@@ -5,35 +5,45 @@
 # or press too late they lose points.
 
 score = 0
+hiscore = 0
 letter_showing = ""
 isdead = False
 lives = 3
+max_time_on_screen=0
+max_pause_before_showing=0
+
+game_reset()
 
 def on_button_pressed_common(letter_2_check):
-    global letter_showing, score, lives
+    global letter_showing, score, lives, max_time_on_screen,max_pause_before_showing
     if letter_showing == letter_2_check:
         letter_showing = ""
         basic.clear_screen()
         music.play_tone(462, music.beat(BeatFraction.WHOLE))
         score = score + 1
+        max_time_on_screen = max_time_on_screen / 2
+        max_pause_before_showing = max_pause_before_showing / 2
     else:
         music.play_tone(100 + lives * 20, music.beat(BeatFraction.WHOLE))
         lives = lives - 1
-        check_if_dead() 
+        check_if_dead()
 
 def game_reset():
-    global score,letter_showing,isdead,lives
+    global score,letter_showing,isdead,lives,max_time_on_screen,max_pause_before_showing
     score = 0
     letter_showing = ""
-    lives = 3
     basic.show_icon(IconNames.HAPPY)
     music.play_melody("C5 D D F D F F D ", 120)
+    lives = 3
     isdead = False
+    max_time_on_screen = 4000
+    max_pause_before_showing = 2000
 
-def reset_game():
+
+def on_ab_pressed():
     if isdead:
         game_reset()
-input.on_button_pressed(Button.AB,reset_game)
+input.on_button_pressed(Button.AB, on_ab_pressed)
 
 def on_button_pressed_a():
    on_button_pressed_common("A")
@@ -44,31 +54,29 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def check_if_dead():
-    global lives, isdead, score
+    global lives, isdead, score, hiscore
     if lives <= 0:
         # Yes, we're dead.
         isdead = True
         basic.show_icon(IconNames.GHOST)
-        #music.play_melody("C5 A B G A F G E ", 120)
         basic.pause(2000)
-        basic.show_number(score)
-
+        basic.show_string("Score: " + score)
+        if score > hiscore:
+            hiscore = score
+        basic.show_string("Hi:" + hiscore)
 
 def on_forever():
-    global letter_showing, isdead
+    global letter_showing, isdead, max_pause_before_showing, max_time_on_screen
     if not isdead:
-        basic.pause(randint(0, 2000))
+        basic.pause(randint(0, max_pause_before_showing))
         if Math.random_boolean():
             letter_showing = "A"
             basic.show_string("A")
         else:
             letter_showing = "B"
             basic.show_string("B")
-        basic.pause(1000)
+        basic.pause(randint(0, max_time_on_screen))
         basic.clear_screen()
         letter_showing = ""
-    else:
-        pass
-        # what should we do when we're dead?
 
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -4,9 +4,13 @@
 //  time to score a point.  If they press the wrong button
 //  or press too late they lose points.
 let score = 0
+let hiscore = 0
 let letter_showing = ""
 let isdead = false
 let lives = 3
+let max_time_on_screen = 0
+let max_pause_before_showing = 0
+game_reset()
 function on_button_pressed_common(letter_2_check: string) {
     
     if (letter_showing == letter_2_check) {
@@ -14,6 +18,8 @@ function on_button_pressed_common(letter_2_check: string) {
         basic.clearScreen()
         music.playTone(462, music.beat(BeatFraction.Whole))
         score = score + 1
+        max_time_on_screen = max_time_on_screen / 2
+        max_pause_before_showing = max_pause_before_showing / 2
     } else {
         music.playTone(100 + lives * 20, music.beat(BeatFraction.Whole))
         lives = lives - 1
@@ -26,13 +32,15 @@ function game_reset() {
     
     score = 0
     letter_showing = ""
-    lives = 3
     basic.showIcon(IconNames.Happy)
     music.playMelody("C5 D D F D F F D ", 120)
+    lives = 3
     isdead = false
+    max_time_on_screen = 4000
+    max_pause_before_showing = 2000
 }
 
-input.onButtonPressed(Button.AB, function reset_game() {
+input.onButtonPressed(Button.AB, function on_ab_pressed() {
     if (isdead) {
         game_reset()
     }
@@ -50,18 +58,21 @@ function check_if_dead() {
         //  Yes, we're dead.
         isdead = true
         basic.showIcon(IconNames.Ghost)
-        // music.play_melody("C5 A B G A F G E ", 120)
         basic.pause(2000)
-        basic.showNumber(score)
+        basic.showString("Score: " + score)
+        if (score > hiscore) {
+            hiscore = score
+        }
+        
+        basic.showString("Hi:" + hiscore)
     }
     
 }
 
-//  what should we do when we're dead?
 basic.forever(function on_forever() {
     
     if (!isdead) {
-        basic.pause(randint(0, 2000))
+        basic.pause(randint(0, max_pause_before_showing))
         if (Math.randomBoolean()) {
             letter_showing = "A"
             basic.showString("A")
@@ -70,11 +81,9 @@ basic.forever(function on_forever() {
             basic.showString("B")
         }
         
-        basic.pause(1000)
+        basic.pause(randint(0, max_time_on_screen))
         basic.clearScreen()
         letter_showing = ""
-    } else {
-        
     }
     
 })
